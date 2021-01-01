46a95f9e7fd888b9832b070ae87d531c1b81ee79
FSuaverdez <sfrannz@gmail.com>
2021-01-01 19:18:02 +0800

    Added Product and Rating Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import data from './data'
-
+import Product from './components/Product'
 function App() {
   return (
     <div className='grid-container'>
       <header className='row'>
         <div>
           <a href='index.html' className='brand'>
-            Marketplace asd
+            SuaBurgers
           </a>
         </div>
         <div>
@@ -17,38 +17,7 @@ function App() {
       <main>
         <div className='row center'>
           {data.products.map((product) => (
-            <div key={product._id} className='card'>
-              <a href={`/product/${product._id}`}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className='medium'
-                />
-              </a>
-              <div className='card-body'>
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className='rating'>
-                  <span>
-                    <i className='fa fa-star'></i>
-                  </span>
-                  <span>
-                    <i className='fa fa-star'></i>
-                  </span>
-                  <span>
-                    <i className='fa fa-star'></i>
-                  </span>
-                  <span>
-                    <i className='fa fa-star'></i>
-                  </span>
-                  <span>
-                    <i className='fa fa-star'></i>
-                  </span>
-                </div>
-                <div className='price'>${product.price}</div>
-              </div>
-            </div>
+            <Product key={product._id} product={product} />
           ))}
         </div>
       </main>

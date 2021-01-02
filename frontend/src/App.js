8b74cb9689fdb49c24a1b4cb00fbf8af61ebd992
FSuaverdez <sfrannz@gmail.com>
2021-01-02 13:55:53 +0800

    Finished Session 7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,29 @@
-import data from './data'
-import Product from './components/Product'
+import { BrowserRouter, Route } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen'
+import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
-    <div className='grid-container'>
-      <header className='row'>
-        <div>
-          <a href='index.html' className='brand'>
-            SuaBurgers
-          </a>
-        </div>
-        <div>
-          <a href='/cart'>Cart</a>
-          <a href='/signin'>Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className='row center'>
-          {data.products.map((product) => (
-            <Product key={product._id} product={product} />
-          ))}
-        </div>
-      </main>
+    <BrowserRouter>
+      <div className='grid-container'>
+        <header className='row'>
+          <div>
+            <a href='index.html' className='brand'>
+              SuaBurgers
+            </a>
+          </div>
+          <div>
+            <a href='/cart'>Cart</a>
+            <a href='/signin'>Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path='/' component={HomeScreen} exact></Route>
+        </main>
 
-      <footer className='row center'>All Right Reserved</footer>
-    </div>
+        <footer className='row center'>All Right Reserved</footer>
+      </div>
+    </BrowserRouter>
   )
 }
 

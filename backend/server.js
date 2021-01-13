1105b7a617076c5cba3b89ebc47b03f47247cb57
FSuaverdez <sfrannz@gmail.com>
2021-01-13 09:33:50 +0800

    Updated to Session 28

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,36 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import productRouter from './routers/productRouter.js';
-import userRouter from './routers/userRouter.js';
-const app= express();
+import express from 'express'
+import mongoose from 'mongoose'
+import productRouter from './routers/productRouter.js'
+import userRouter from './routers/userRouter.js'
+import dotenv from 'dotenv'
+import orderRouter from './routers/orderRouter.js'
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/marketplace', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-});
+})
 
+app.use('/api/users', userRouter)
+app.use('/api/products', productRouter)
+app.use('/api/orders', orderRouter)
 
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
+})
 
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
-
-app.get('/', (req, res)=>{
-    res.send('Server is ready');
-});
+app.get('/', (req, res) => {
+  res.send('Server is ready')
+})
 //middleware/error catcher
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message });
-  });
-const port= process.env.PORT || 3001;
-app.listen(port, ()=>{
-    console.log(`serve at http://localhost:${port}`);
-});
+  res.status(500).send({ message: err.message })
+})
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`)
+})
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,29 @@
 
 import {BrowserRouter,Route, Link} from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import { signout } from './actions/userActions';
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
+
+
 function App() {
 
   //Adding of badge
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter> 
     <div className="grid-container">
@@ -29,13 +43,36 @@ function App() {
           <span className="badge">{cartItems.length}</span>
         )}
       </Link>
-      <Link to="/signin">Sign In</Link>
+
+      {userInfo ? (
+        <div className="dropdown">
+          <Link to="#">
+            {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+          </Link>
+          <ul className="dropdown-content">
+            <li>
+              <Link to="#signout" onClick={signoutHandler}>
+                Sign Out
+              </Link>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <Link to="/signin">Sign In</Link>
+      )}
     </div>
   </header>
         <main>
         <Route path="/cart/:id?" component={CartScreen}></Route>
         <Route path="/product/:id" component={ProductScreen}></Route>
-          <Route path="/" component={HomeScreen}exact ></Route>
+        <Route path="/signin" component={SigninScreen}></Route>
+        <Route path="/register" component={RegisterScreen}></Route>
+        <Route path="/shipping" component={ShippingAddressScreen}></Route>
+        <Route path="/payment" component={PaymentMethodScreen}></Route>
+        <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+        <Route path="/order/:id" component={OrderScreen}></Route>
+        <Route path="/" component={HomeScreen}exact ></Route>
+        
           
         </main>
         <footer className="row center"> All rights reserved.
